6e7ec5b3e32185594fe0854f9e5759892c7cd5e8
follijulio <follijulio@gmail.com>
2025-01-16 13:04:55 -0300

    atualiza o componente FlipCard para melhorar a exibição de flashcards com novos estilos e informações

--- src/app/ui/FlipCard.tsx
+++ src/app/ui/FlipCard.tsx
@@ -25,14 +25,20 @@ const FlipCard: React.FC<FlipCardProps> = ({ flashcard }) => {
       onClick={handleClick}
     >
       <div className={`flip-card ${isFlipped ? "flipped" : ""}`}>
-        <Card className="flip-card-front">
+        <Card className="flip-card-front flex flex-col">
+          <div className="w-full flex justify-between items-center"> 
+          <h2>
+            {flashcard.materia}
+          </h2>
+          <span className={`h-4 w-10 rounded-full bg-[#ffffff] flex justify-center items-center text-xs text-black`}>{flashcard.nivel.dificuldade}</span>
+          </div>
           <p>
-            {flashcard.pergunta}
+            {flashcard.pergunta} - FRENTE
           </p>
         </Card>
         <Card className="flip-card-back">
           <p>
-            {flashcard.resposta}
+            {flashcard.resposta} - FUNDO  
           </p>
         </Card>
       </div>
@@ -43,11 +49,13 @@ const FlipCard: React.FC<FlipCardProps> = ({ flashcard }) => {
 const Card: React.FC<cardProps> = ({ className, children }) => {
   return (
     <div
-      className={` w-full h-full border-2 rounded-xl border-white  ${className}`}
+      className={` w-full h-full border-2 rounded-xl border-white bg-gradient-to-tr from-blue-900 to-slate-600 p-12 text-white text-3xl select-none ${className}`}
     >
       {children}
     </div>
   );
 };
+
+
 
 export default FlipCard;
